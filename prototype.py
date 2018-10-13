924c3d556e2212d30adfc408815349f3985034d6
swandro <stevewandro@gmail.com>
2018-10-12 17:15:14 -0700

    Got list of all genes

--- prototype.py
+++ prototype.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 from Bio import AlignIO
+from Bio import Seq
 from Bio.Blast.Applications import NcbiblastxCommandline
 import sys
 import os
@@ -7,21 +8,38 @@ import pathlib
 import glob
 import numpy
 
-###Code to run a blast
+#######Code to run a blast#########
 test = NcbiblastxCommandline(cmd="blastn.exe", out="test.txt", outfmt=5, query="seq.txt", db="db")
 test()
 
+'''
+notes
+a.seq = Seq object with sequence
+a.features = list of SeqFeature
+SeqFeature.qualifiers = dictionary of anotations
+SeqFeature.qualifiers["db_xref"] or ["translation"] or ["product"]
+'''
 
-#Importing genbank files
-gbk_files = []
-genome_file_list = glob.glob('genomes/*.gb')
+
+
+######Importing genbank files
+
+#Get a list of all Genbank files
+gbk_files = [] #make empty list
+genome_file_list = glob.glob('genomes/*.gb')   ##Get names of all genomes
+
+#Loop through and add genbank files to gbk_files
 for genome in genome_file_list:
     with open(genome,   'r') as genbank_file:
-        temp = SeqIO.parse(genbank_file, "genbank")
+        for redord in SeqIO.parse(genbank_file, "genbank"):
+            gbk_files.append(redord)
 
-for record in gbk_files:
-    print(record)
 
-    with open("cor6_6.gb", "rU") as input_handle:
-        for record in SeqIO.parse(input_handle, "genbank"):
-            print(record)
+#Get a list of all genes
+all_genes = []
+for genome in gbk_files:
+    for gene in genome.features:
+        if gene.type in ["CDS", "tRNA"]:   ##only grabbing CDS and tRNA for now
+            gene.seq = gene.extract(genome.seq)  ##Add the sequence to each gene
+            all_genes.append(gene)  ##Add gene to list
+
